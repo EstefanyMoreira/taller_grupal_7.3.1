eadc902f7479210878ccdd5a8795d91921f964f4
estefanymoreira <estefanymoreira06@gmail.com>
2024-08-15 13:59:16 -0300

    minimo de password

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,12 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function checkPasswords() {
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+
+    if (password1 !== password2) {
+        "alert-danger";
+    }
+}
