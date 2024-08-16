d80ae179bb816c03e180815d3fccb344e86a2002
FlorenciaProletti <florenciaproletti@gmail.com>
2024-08-16 20:20:06 -0300

    Solucion ejercicio

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,4 @@
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -6,11 +7,48 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-function checkPasswords() {
-    const password1 = document.getElementById('password1').value;
-    const password2 = document.getElementById('password2').value;
 
-    if (password1 !== password2) {
-        "alert-danger";
-    }
-}
+let boton = document.getElementById('regBtn');
+
+boton.addEventListener ("click", function () {
+
+let nombre = document.getElementById('nombre').value;
+let apellido= document.getElementById('apellido').value;
+let email= document.getElementById('email').value;
+let password1= document.getElementById('password1').value;
+let password2= document.getElementById('password2').value;
+let terminos = document.getElementById('terminos');
+
+ if (nombre == '' || apellido == '' || email =='' || password1 == '' || password2 == '' || !terminos.checked) {
+    showAlertError();
+
+} else if ( password1.length < 6 ) {
+
+    showAlertError(); } 
+
+    else if (password1 !== password2 ) {
+
+    showAlertError();
+
+} else { showAlertSuccess();
+}}
+
+)  
+
+
+//function showAlertSuccess() {
+// document.getElementById("alert-success").classList.add("show");
+//}
+
+//function showAlertError() {
+//  document.getElementById("alert-danger").classList.add("show");
+//}
+
+//function checkPasswords() {
+  //  const password1 = document.getElementById('password1').value;
+   // const password2 = document.getElementById('password2').value;
+
+ //   if (password1 !== password2) {
+  //      "alert-danger";
+  //  }
+//}
