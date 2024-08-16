a29fc3ae661bb62567e832dbfcb0db06c84c8cb3
estefanymoreira <estefanymoreira06@gmail.com>
2024-08-16 20:36:20 -0300

    borrado comentario

--- js/validacion.js
+++ js/validacion.js
@@ -34,21 +34,3 @@ let terminos = document.getElementById('terminos');
 }}
 
 )  
-
-
-//function showAlertSuccess() {
-// document.getElementById("alert-success").classList.add("show");
-//}
-
-//function showAlertError() {
-//  document.getElementById("alert-danger").classList.add("show");
-//}
-
-//function checkPasswords() {
-  //  const password1 = document.getElementById('password1').value;
-   // const password2 = document.getElementById('password2').value;
-
- //   if (password1 !== password2) {
-  //      "alert-danger";
-  //  }
-//}
